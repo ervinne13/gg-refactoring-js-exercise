31b886481fad518aa0c984a5c51ceb2930bc0301
Ervinnne Sodusta <ervinnesodusta13@yahoo.com.ph>
2019-02-04 00:21:54 +0800

    Add test with action

--- tests/agenda-display-test.js
+++ tests/agenda-display-test.js
@@ -11,8 +11,18 @@ fixture `Agenda Display Test`
     .requestHooks(mock)
 
 test("Agenda should display on page ready", async t => {
+    let stubTitle = AgendStub[0].title;
     await t
         .expect(Selector('#agenda-timeline').innerText).notEql('')
-        .expect(Selector('#agenda-timeline .vertical-timeline-content h3').innerText).eql(AgendStub[0].title)
+        .expect(Selector('#agenda-timeline .vertical-timeline-content h3').innerText).eql(stubTitle)
+        // .debug() //  for when we want to pause the test
+})
+
+test("Agenda details should display on click view details", async t => {
+    let stubId = AgendStub[0].id;
+    let stubContent = AgendStub[0].content;
+    await t
+        .click(`button[data-id="${stubId}"]`)
+        .expect(Selector('#agenda-details-container').innerText).contains(stubContent)
         // .debug() //  for when we want to pause the test
 })
