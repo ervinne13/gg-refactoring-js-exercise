adc515e9493aa7f705c58f2533176fe6761c55b4
Ervinnne Sodusta <ervinnesodusta13@yahoo.com.ph>
2019-02-10 05:16:23 +0800

    Add strict mode

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,14 @@ module.exports = {
     "env": {
         "browser": true,
         "es6": true,
-        "node": true
+        "node": true,        
+    },
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
     },
     "globals": {
         "AgendaAPI": "readable",        
--- public/assets/js/agenda-page.js
+++ public/assets/js/agenda-page.js
@@ -1,4 +1,5 @@
 /* global tmpl:readable */
+"use strict";
 
 document.addEventListener('DOMContentLoaded', () => {
     AgendaPage.loadAndDisplayAgendas();
